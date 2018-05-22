f074bec83e0204b7d5abcc3e9ecd05ccad663ee9
sdevan <stevendevan12@gmail.com>
2018-05-21 22:23:02 -0700

    Initial commit. Dedicated data prep script.

---
+++ generate_working_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import pickle as pkl
+
+import edatools
+
+infolder = 'data/input/'
+outfolder = 'data/working/'
+df_train = pd.read_csv(infolder + 'train.csv')
+df_test = pd.read_csv(infolder + 'test.csv')
+
+# Remove 2 outliers as recommended by
+# http://ww2.amstat.org/publications/jse/v19n3/Decock/DataDocumentation.txt
+df_train = df_train.drop(df_train[(df_train['GrLivArea'] > 4000) &
+                                  (df_train['SalePrice'] < 300000)].index)
+ntrain = df_train.shape[0]
+
+y = df_train['SalePrice']
+df_train.drop(['SalePrice'], axis=1, inplace=True)
+df_all = pd.concat([df_train, df_test], ignore_index=True)
+
+df_all = edatools.condition_housing_data(df_all)
+
+df_train = df_all[:ntrain]
+df_test = df_all[ntrain:].reset_index(drop=True)
+
+df_test = pd.concat([df_test, y], axis=1)
+
+df_train.to_csv(outfolder + 'train_cleaned_79features.csv', index=False)
+df_test.to_csv(outfolder + 'test_cleaned_79features.csv', index=False)
