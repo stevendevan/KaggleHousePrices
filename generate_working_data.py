84def6a99736b7bbd4f20f255dc43e6c2334746c
sdevan <stevendevan12@gmail.com>
2018-05-21 23:46:30 -0700

    Fixed bug in data gen script with inplace row dropping

--- generate_working_data.py
+++ generate_working_data.py
@@ -10,8 +10,11 @@ df_test = pd.read_csv(infolder + 'test.csv')
 
 # Remove 2 outliers as recommended by
 # http://ww2.amstat.org/publications/jse/v19n3/Decock/DataDocumentation.txt
-df_train = df_train.drop(df_train[(df_train['GrLivArea'] > 4000) &
-                                  (df_train['SalePrice'] < 300000)].index)
+
+df_train.drop(df_train[(df_train['GrLivArea'] > 4000) &
+                       (df_train['SalePrice'] < 300000)].index,
+              inplace=True)
+df_train.reset_index(inplace=True, drop=True)
 ntrain = df_train.shape[0]
 
 y = df_train['SalePrice']
@@ -23,7 +26,7 @@ df_all = edatools.condition_housing_data(df_all)
 df_train = df_all[:ntrain]
 df_test = df_all[ntrain:].reset_index(drop=True)
 
-df_test = pd.concat([df_test, y], axis=1)
+df_train = pd.concat([df_train, y], axis=1)
 
 df_train.to_csv(outfolder + 'train_cleaned_79features.csv', index=False)
 df_test.to_csv(outfolder + 'test_cleaned_79features.csv', index=False)
