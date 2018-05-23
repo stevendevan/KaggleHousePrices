9b0b3fa4359e0e6466f563f3809cfe9c40fe76bf
sdevan <stevendevan12@gmail.com>
2018-05-22 22:10:36 -0700

    Version for submission 3, ridge with normalized data.

--- analysis_linear.py
+++ analysis_linear.py
@@ -1,53 +1,55 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, RidgeCV
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 import edatools
 
-df_train = pd.read_csv('data/working/train_cleaned_79features.csv')
-df_test = pd.read_csv('data/working/test_cleaned_79features.csv')
+datafolder = 'data/working/'
+outfolder = 'data/output/'
+df_train = pd.read_csv(datafolder + 'train_normalized.csv')
+df_test = pd.read_csv(datafolder + 'test_normalized.csv')
 
-categoricals = ['MSSubClass', 'MSZoning', 'LotShape', 'LandContour',
-                'Neighborhood', 'HouseStyle', 'Exterior1st', 'Exterior2nd',
-                'MasVnrType', 'ExterQual', 'Foundation', 'BsmtQual',
-                'BsmtExposure', 'HeatingQC', 'CentralAir', 'KitchenQual',
-                'GarageType', 'GarageFinish']
-linear_vars = ['GrLivArea', 'FullBath', 'GarageArea']
-poly_vars = ['OverallQual', 'TotalBsmtSF']
+#categoricals = ['MSSubClass', 'MSZoning', 'LotShape', 'LandContour',
+#                'Neighborhood', 'HouseStyle', 'Exterior1st', 'Exterior2nd',
+#                'MasVnrType', 'ExterQual', 'Foundation', 'BsmtQual',
+#                'BsmtExposure', 'HeatingQC', 'CentralAir', 'KitchenQual',
+#                'GarageType', 'GarageFinish']
+#linear_vars = ['GrLivArea', 'FullBath', 'GarageArea']
+#poly_vars = ['OverallQual', 'TotalBsmtSF']
 
-df_categorical = pd.get_dummies(df_all[categoricals], drop_first=True)
-df_poly = edatools.fill_poly_features(df_all[poly_vars], 2)
-df_linear = df_all[linear_vars]
-
-df_all_clean = pd.concat([df_categorical, df_poly, df_linear], axis=1)
-Xtrain = df_all_clean[:ntrain]
-Xtest = df_all_clean[ntrain:]
-
+Xtrain = df_train.drop(['SalePrice','Id'], axis=1)
+Xtest = df_test.drop(['Id'], axis=1)
+y = np.log1p(df_train['SalePrice'])
 
 model_linear = LinearRegression(normalize=True).fit(Xtrain, y)
 model_lasso = Lasso(alpha=.0001, normalize=True, max_iter=1e5)
-model_ridge = Ridge(alpha=.001, normalize=True, max_iter=1e5)
+model_ridge = Ridge(alpha=.1, normalize=True, max_iter=1e5)
 yfit = model_linear.predict(Xtest)
 
-sns.distplot(y, hist=False)
-sns.distplot(yfit, hist=False)
-plt.show()
+#sns.distplot(y, hist=False)
+#sns.distplot(yfit, hist=False)
+#plt.show()
 
-randstate = 1
+randstate = 3
 scores = cross_val_score(model_ridge,
                          Xtrain.sample(frac=1, random_state=randstate),
                          y.sample(frac=1, random_state=randstate),
                          cv=5,
                          scoring='neg_mean_squared_log_error')
 
+#model_ridgecv = RidgeCV(alphas=[.0001, .001, .01, .1, 1.0, 10.0],
+#				 		normalize=True,
+#                 		cv=5,
+#                 		scoring='neg_mean_squared_log_error')
+
+#model_ridgecv.fit(Xtrain.sample(frac=1, random_state=randstate),
+#                  y.sample(frac=1, random_state=randstate))
+
 model_ridge.fit(Xtrain, y)
 predictions = model_ridge.predict(Xtest)
 
-submission = pd.DataFrame({'Id':df_test.Id, 'SalePrice':predictions})
-submission.to_csv('submission1_ridge.csv', index=False)
+submission = pd.DataFrame({'Id':df_test.Id, 'SalePrice':np.expm1(predictions)})
+submission.to_csv(outfolder + 'submission3_ridge_normalized.csv', index=False)
